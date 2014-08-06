49838c70d588ead0e424d163888e229d04bc8599
Zan Thrash <zanthrash@gmail.com>
2014-08-05 20:21:55 -0500

    Clean up service

--- app/scripts/services/CitySearchService.js
+++ app/scripts/services/CitySearchService.js
@@ -1,18 +1,6 @@
 (function () {
   'use strict';
 
-  function CityFinderService($http, $log, SearchURL) {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-
-    this.findCityInfo = function (q) {
-      return $http.jsonp(SearchURL, {params: {query:q, cb: 'JSON_CALLBACK'}, headers: {"Content-Type": "application/json"} })
-        .then ( function (response) {
-          $log.debug('findCityInfo', response.data);
-          return response.data.RESULTS;
-      });
-    }
-  }
-
   function CitySearchProvider() {
     this.searchURL = null;
 
@@ -37,7 +25,6 @@
   angular
     .module('cityFinder', [])
     .provider('CitySearchService', CitySearchProvider);
-//    .service('CityFinderService', CityFinderService );
 
 })();
 
